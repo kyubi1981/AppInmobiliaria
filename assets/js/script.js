b0935362b0b581d0390cce2365659c94d1b80635
Antonio Rodriguez Rodriguez <rodriguezdomingoantonio@gmail.com>
2025-11-24 00:19:10 -0300

    Desafio4 - Arreglos&Objetos - inmobiliaria, avance al 40%

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+
+
+const cardTemplate (tarjeta){
+    const template = `
+    
+    `;
+}
+
+const listaPropiedades = [
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Apartamento de lujo en zona exclusiva",
+        tipPropiedad: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        direccionPropiedad: "123 Luxury Lane, Prestige Suburb, CA 45678",
+        habitacionesPropiedad: 4,
+        baniosPropiedad: 2,
+        precioPropiedad: 20000000,
+        fumar: false,
+        mascota: false,
+        modalidadPropiedad: "Venta"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Apartamento acogedor en la montaña",
+        tipPropiedad: "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+        direccionPropiedad: "789 Mountain Road, Summit Peaks, CA 23456",
+        habitacionesPropiedad: 3,
+        baniosPropiedad: 2,
+        precioPropiedad: 450000,
+        fumar: true,
+        mascota: false,
+        modalidadPropiedad: "Alquiler"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Penthouse de lujo con terraza panorámica",
+        tipPropiedad: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        direccionPropiedad: "",
+        habitacionesPropiedad: 5,
+        baniosPropiedad: 3,
+        precioPropiedad: 80000000,
+        fumar: true,
+        mascota: true,
+        modalidadPropiedad: "Venta"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Apartamento en el centro de la ciudad",
+        tipPropiedad: "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+        direccionPropiedad: "123 Main Street, Anytown, CA 91234",
+        habitacionesPropiedad: 2,
+        baniosPropiedad: 1,
+        precioPropiedad: 300000,
+        fumar: false,
+        mascota: true,
+        modalidadPropiedad: "Alquiler"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Hermoso Departamento cercano a todo lo que buscas",
+        tipPropiedad: "Un lugar increible, areas verdes, gimnasio, quincho, terraza. Con una conectividad y a pasos del Mall Central",
+        direccionPropiedad: "Estación Alameda 365, Santiago Centro, Chile",
+        habitacionesPropiedad: 2,
+        baniosPropiedad: 2,
+        precioPropiedad: 500000,
+        fumar: true,
+        mascota: true,
+        modalidadPropiedad: "Alquiler"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "",
+        tipPropiedad: "",
+        direccionPropiedad: "",
+        habitacionesPropiedad: "",
+        baniosPropiedad: "",
+        precioPropiedad: "",
+        fumar: false,
+        mascota: false,
+        modalidadPropiedad: "Venta"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Apartamento en el centro de la ciudad",
+        tipPropiedad: "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+        direccionPropiedad: "123 Main Street, Anytown, CA 91234",
+        habitacionesPropiedad: 2,
+        baniosPropiedad: 1,
+        precioPropiedad: 300000,
+        fumar: false,
+        mascota: true,
+        modalidadPropiedad: "Alquiler"
+    },
+    {
+        imgPropiedad: "",
+        tituloPropiedad: "Hermoso Departamento cercano a todo lo que buscas",
+        tipPropiedad: "Un lugar increible, areas verdes, gimnasio, quincho, terraza. Con una conectividad y a pasos del Mall Central",
+        direccionPropiedad: "Estación Alameda 365, Santiago Centro, Chile",
+        habitacionesPropiedad: 2,
+        baniosPropiedad: 2,
+        precioPropiedad: 500000,
+        fumar: true,
+        mascota: true,
+        modalidadPropiedad: "Alquiler"
+    }
+]
